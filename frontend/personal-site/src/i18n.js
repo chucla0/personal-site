d15567f1fa83b6714acc7371e4ed036188daea0f
chuclao <a23izadelesp@inspedralbes.cat>
2025-11-20 12:37:48 +0100

    feat: Introduce Catalan locale and correct email placeholder and apostrophe escaping in English and Spanish translations.

--- frontend/personal-site/src/i18n.js
+++ frontend/personal-site/src/i18n.js
@@ -1,12 +1,12 @@
 import { createI18n } from 'vue-i18n'
 import esMessages from './locales/es.json?raw'
+import caMessages from './locales/ca.json?raw'
 import enMessages from './locales/en.json?raw'
-// import caMessages from './locales/ca.json?raw'
 
 const messages = {
   es: JSON.parse(esMessages),
+  ca: JSON.parse(caMessages),
   en: JSON.parse(enMessages)
-  // ca: JSON.parse(caMessages),
 }
 
 const i18n = createI18n({
